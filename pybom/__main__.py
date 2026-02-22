c06c4026271d8454a658410128fa56b8b11eb471
Rob Siegwart <rob@siegwart.me>
2026-02-21 21:37:20 -0600

    Refactor main function in __main__.py and update pyproject.toml for improved structure and dependencies

--- pybom/__main__.py
+++ pybom/__main__.py
@@ -8,42 +8,43 @@ Run the program from the command line via python module mode.
 
 '''
 
-import sys
 import argparse
 from .BOM import BOM
 
 
-parser = argparse.ArgumentParser(
-    prog='python -m pyBOM',
-    description='Parse a folder of Excel Bill-of-Materials.'
-)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='pybom',
+        description='Parse a folder of Excel Bill-of-Materials.'
+    )
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '-f', '--file',
+        help='The name of a single Excel BOM file.',
+        metavar='FILE'
+    )
+    group.add_argument(
+        '-d', '--dir',
+        help='The name of the folder containing Excel BOM files.',
+        metavar='FOLDER'
+    )
+
+    parser.add_argument(
+        'action',
+        help='What to do with the resulting BOM.',
+        default='tree'
+    )
+
+    ns = parser.parse_args()
+
+    if ns.file:
+        bom = BOM.single_file(ns.file)
+    elif ns.dir:
+        bom = BOM.from_folder(ns.dir)
+
+    print(getattr(bom, ns.action))
 
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument(
-    '-f', '--file',
-    help='The name of a single Excel BOM file.',
-    metavar='FILE'
-)
-group.add_argument(
-    '-d', '--dir',
-    help='The name of the folder containing Excel BOM files.',
-    metavar='FOLDER'
-)
-
-parser.add_argument(
-    'action',
-    help='What to do with the resulting BOM.',
-    default='tree'
-)
-
-ns = parser.parse_args()
-
-if ns.file:
-    bom = BOM.single_file(ns.file)
-elif ns.dir:
-    bom = BOM.from_folder(ns.dir)
-else:
-    raise ValueError('Either --file or --dir must be specified.')
-
-print(getattr(bom, ns.action))
+if __name__ == '__main__':
+    main()
